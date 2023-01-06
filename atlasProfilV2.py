484eed489eda109d5798f60fab8b3cb01fe43fe0
guibros <guillaumebrossard04@gmail.com>
2023-01-06 14:26:09 -0500

    db test

--- atlasProfilV2.py
+++ atlasProfilV2.py
@@ -75,75 +75,75 @@ class RPA:
         return self.collection.find_one({"ClientInfo.keyName": keyName})
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    # INSTANTIATE CLASS
-    RPA_db = RPA()
+#     # INSTANTIATE CLASS
+#     RPA_db = RPA()
     
-    # INSERT CLIENT WITH METHOD
-    RPA_db.insertNewClient( 'John01', 'John', 'Doe', '514-564-8656', '123 RPA street, Montreal, Quebec, H1H 1H1',
-                            friend_1='Nancy Smith', fr_phone_1='514-222-3456',
-                            friend_2='Harry Klein', fr_phone_2='514-333-7547',
-                            friend_3='George Ramos', fr_phone_3='514-444-8685',
-                            family_1='Jim Doe', fa_phone_1='514-555-5745',
-                            family_2='Larry Doe', fa_phone_2='514-666-9867',
-                            family_3='Stephanie Doe', fa_phone_3='514-777-4354',
-                            medical_1='Dr. Miles', me_phone_1='514-888-9876',
-                            medical_2='Dr. Kravitz', me_phone_2='514-234-2452',
-                            medical_3='Dr. Nancy', me_phone_3='514-432-7355',
-                            emergency_1='Billy Doe', em_phone_1='514-567-2432',
-                            emergency_2='Evelyne Richards', em_phone_2='765-111-7546',
-                            emergency_3='Lisa Kuds', em_phone_3='795-564-1234',
-                            medication_1='Valium XR', dosageMed_1='3 times per day for 2 weeks',
-                            medication_2='Motrin', dosageMed_2='1 pill every morning',
-                            medication_3='Nalaproxen', dosageMed_3='2 pills before bed',
-                            appointment_1='Oncologist', date_1='Monday, February 4, 2023', time_1='14:00', place_1='CLSC Montreal',
-                            appointment_2='Neurosurgeon', date_2='Tuesday, March 6, 2023', time_2='13h00', place_2='Notre-Dame Hospital',
-                            appointment_3='Family doctor', date_3='Thursday, April 2, 2023', time_3='8h30', place_3='Downtown Medical Clinic')
+#     # INSERT CLIENT WITH METHOD
+#     RPA_db.insertNewClient( 'John01', 'John', 'Doe', '514-564-8656', '123 RPA street, Montreal, Quebec, H1H 1H1',
+#                             friend_1='Nancy Smith', fr_phone_1='514-222-3456',
+#                             friend_2='Harry Klein', fr_phone_2='514-333-7547',
+#                             friend_3='George Ramos', fr_phone_3='514-444-8685',
+#                             family_1='Jim Doe', fa_phone_1='514-555-5745',
+#                             family_2='Larry Doe', fa_phone_2='514-666-9867',
+#                             family_3='Stephanie Doe', fa_phone_3='514-777-4354',
+#                             medical_1='Dr. Miles', me_phone_1='514-888-9876',
+#                             medical_2='Dr. Kravitz', me_phone_2='514-234-2452',
+#                             medical_3='Dr. Nancy', me_phone_3='514-432-7355',
+#                             emergency_1='Billy Doe', em_phone_1='514-567-2432',
+#                             emergency_2='Evelyne Richards', em_phone_2='765-111-7546',
+#                             emergency_3='Lisa Kuds', em_phone_3='795-564-1234',
+#                             medication_1='Valium XR', dosageMed_1='3 times per day for 2 weeks',
+#                             medication_2='Motrin', dosageMed_2='1 pill every morning',
+#                             medication_3='Nalaproxen', dosageMed_3='2 pills before bed',
+#                             appointment_1='Oncologist', date_1='Monday, February 4, 2023', time_1='14:00', place_1='CLSC Montreal',
+#                             appointment_2='Neurosurgeon', date_2='Tuesday, March 6, 2023', time_2='13h00', place_2='Notre-Dame Hospital',
+#                             appointment_3='Family doctor', date_3='Thursday, April 2, 2023', time_3='8h30', place_3='Downtown Medical Clinic')
     
-# GET ALL CONTACT INFO BASED ON MQTT KEY RECEIVED AND PLACE IN VARIABLE 'john'
-john = RPA_db.findClient('John01')
+# # GET ALL CONTACT INFO BASED ON MQTT KEY RECEIVED AND PLACE IN VARIABLE 'john'
+# john = RPA_db.findClient('John01')
 
 
-#ACCESS DATA (SEE EXAMPLES BELOW)
+# #ACCESS DATA (SEE EXAMPLES BELOW)
     
-# ALL DATA
-print('\n\n**************** ALL DATA ***************')
-print(f"Client data:\n\n{john}")
+# # ALL DATA
+# print('\n\n**************** ALL DATA ***************')
+# print(f"Client data:\n\n{john}")
 
-# CONTACT CATEGORIES
-print('\n\n**************** CONTACT CATEGORIES ***************')
-print(list(john['Contacts'].keys()))
+# # CONTACT CATEGORIES
+# print('\n\n**************** CONTACT CATEGORIES ***************')
+# print(list(john['Contacts'].keys()))
 
-# FAMILLY CONTACTS 
-print('\n\n**************** COMPLETE FAMILLY CONTACTS ***************')
-print(list(john['Contacts']['Family'].items()))
+# # FAMILLY CONTACTS 
+# print('\n\n**************** COMPLETE FAMILLY CONTACTS ***************')
+# print(list(john['Contacts']['Family'].items()))
 
-# NAMES OF DOCTORS 
-print('\n\n**************** ONLY NAMES OF DOCTORS ***************')
-print(list(john['Contacts']['Medical'].keys()))
+# # NAMES OF DOCTORS 
+# print('\n\n**************** ONLY NAMES OF DOCTORS ***************')
+# print(list(john['Contacts']['Medical'].keys()))
 
-# ONE EMERGENCY CONTACT 
-print('\n\n**************** ONE EMERGENCY CONTACT ("Billy Doe") ***************')
-print(john['Contacts']['Emergency']['Billy Doe'])
+# # ONE EMERGENCY CONTACT 
+# print('\n\n**************** ONE EMERGENCY CONTACT ("Billy Doe") ***************')
+# print(john['Contacts']['Emergency']['Billy Doe'])
 
-# MEDICATION LIST
-print('\n\n**************** MEDICATION LIST ***************')
-print(list(john['Medication'].keys()))
+# # MEDICATION LIST
+# print('\n\n**************** MEDICATION LIST ***************')
+# print(list(john['Medication'].keys()))
 
-# MEDICATION LIST and DOSAGE
-print('\n\n**************** MEDICATION LIST and DOSAGE ***************')
-print(list(john['Medication'].items()))
+# # MEDICATION LIST and DOSAGE
+# print('\n\n**************** MEDICATION LIST and DOSAGE ***************')
+# print(list(john['Medication'].items()))
 
-# APPOINTMENTS
-print('\n\n**************** APPOINTMENT DATES ***************')
-for appointment, info in john['Appointments'].items():
-    print(appointment, info['Date'], info['Time'], info['Location'])
+# # APPOINTMENTS
+# print('\n\n**************** APPOINTMENT DATES ***************')
+# for appointment, info in john['Appointments'].items():
+#     print(appointment, info['Date'], info['Time'], info['Location'])
     
-# APPOINTMENT LOCATIONS
-print('\n\n**************** ONLY APPOINTMENT LOCATIONS ***************')
-for appointment, info in john['Appointments'].items():
-    print(info['Location'])
+# # APPOINTMENT LOCATIONS
+# print('\n\n**************** ONLY APPOINTMENT LOCATIONS ***************')
+# for appointment, info in john['Appointments'].items():
+#     print(info['Location'])
     
     
-print('\n\n**************** END ***************')
+# print('\n\n**************** END ***************')
--- test3.py
+++ test3.py
@@ -9,3 +9,7 @@ data = nlp.listen()
 nlp.speak(data)
 result = nlp.analyze(data)
 nlp.speak(result)
+
+# if nlp.analyzeVariable(text, friend1):
+#     retuen phone number 
+#     call number associated to friend1
